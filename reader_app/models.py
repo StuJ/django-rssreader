6bda01b1a053ff36435ee5d7a96e3710113501d0
SJ17 <stuartjennings13@gmail.com>
2016-06-14 11:44:10 +0100

    Favourite model added

--- reader_app/models.py
+++ reader_app/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Favourite(models.Model):
+    title = models.CharField(max_length= 200)
+    description = models.CharField(max_length= 2000)
+    link = models.URLField()
+    pubDate = models.DateTimeField()
+
